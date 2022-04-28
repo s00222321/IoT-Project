904bb6c9542dc4c7f6d14f19c8a505cb58c2fec0
ellen-woodward <98536796+ellen-woodward@users.noreply.github.com>
2022-04-28 15:06:35 +0100

    if statement included

--- new library_test/newlibrary_test/newlibrary_test.ino
+++ new library_test/newlibrary_test/newlibrary_test.ino
@@ -8,6 +8,9 @@ rgb_lcd lcd;
 int sensorPin = A0;  
 int sensorValue = 0;
 int sensorPrintValue = 0; 
+int buttonPin = 4;
+
+int buttonState = 0;
 
 #define STEPS_PER_MOTOR_REVOLUTION 32
 #define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
@@ -19,11 +22,16 @@ int  Steps2Take;
 void setup()  
 {
   pinMode(sensorPin, INPUT);
+  pinMode(buttonPin, INPUT);
   lcd.begin(16, 2);
 }
 
 void loop()  
 {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH)
+  {
   sensorValue = analogRead(sensorPin);
   sensorPrintValue = sensorValue / 4.3;
   Serial.print("sensor = ");
@@ -82,5 +90,12 @@ if (sensorValue >= 700)
     small_stepper.step(Steps2Take);
   }
 
+  }
+
+else{
+  small_stepper.setSpeed(0);
+  small_stepper.step(Steps2Take);
+}
+}
 }
 
